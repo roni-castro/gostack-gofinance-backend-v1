116ec1e9029665ea3e1cf130120315dcec8f0d0c
Roni <roni.c.castro@gmail.com>
2020-04-19 20:37:16 -0300

    Add create transaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,13 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import { mapToTransactionType } from '../repositories/transactionTypeMapper';
+import TransactionType from '../models/TransactionType';
+
+interface CreateTransactionRequestDTO {
+  title: string;
+  value: number;
+  type: string;
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,8 +16,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({
+    value,
+    type,
+    title,
+  }: CreateTransactionRequestDTO): Transaction {
+    const transactionType = mapToTransactionType(type);
+    if (transactionType === TransactionType.Outcome) {
+      const balance = this.transactionsRepository.getBalance();
+      if (value > balance.total) {
+        throw new Error('The outcome is bigger than the total value in cash');
+      }
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type: transactionType,
+    });
+    return transaction;
   }
 }
 
